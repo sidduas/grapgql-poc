c639da013e44815fad72c3a2951d9594c56586a4
Siddu <siddu.as@gmail.com>
2023-10-04 01:35:47 +0530

    Initial poc checkin

---
+++ src/main/java/com/mypoc/grapgql/GrapgqlPocApplication.java
@@ -0,0 +1,13 @@
+package com.mypoc.grapgql;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrapgqlPocApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrapgqlPocApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/mypoc/grapgql/GrapgqlPocApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mypoc.grapgql;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GrapgqlPocApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
